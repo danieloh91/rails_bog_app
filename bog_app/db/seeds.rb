372622186022249fef952edb934c9320394cf576
danieloh91 <daniel.oh.91@gmail.com>
2016-08-14 15:04:41 -0700

    index shows

---
+++ bog_app/db/seeds.rb
@@ -0,0 +1,16 @@
+hash = [
+  {
+    name: "yoda",
+    description: "little green guy"
+  },
+  {
+    name: "luke",
+    description: "jedi"
+  },
+  {
+    name: "darth vader",
+    description: "bad guy"
+  }
+]
+
+p Creature.create(hash)

7a7a85c3faf52b54149076a24aece50e08d43dea
danieloh91 <daniel.oh.91@gmail.com>
2016-08-13 23:17:07 -0700

    index works

---
+++ bog_app/db/seeds.rb
@@ -0,0 +1,16 @@
+hash = [
+  {
+    name: "yoda",
+    description: "little green guy"
+  },
+  {
+    name: "luke",
+    description: "jedi"
+  },
+  {
+    name: "darth vader",
+    description: "luke is my son"
+  }
+]
+
+p Creature.create(hash)
